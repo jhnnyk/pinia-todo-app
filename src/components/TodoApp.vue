feeddbde7dd75ad82669da08b1b1d3d55a900f6f
John Knotts <jhnnyk@gmail.com>
2024-07-16 15:41:30 -0600

    initial project setup

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TodoForm from '@/components/TodoForm.vue'
+import TodoList from '@/components/TodoList.vue'
+</script>
+
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <TodoForm />
+    <TodoList />
+  </div>
+</template>
+
+<style scoped></style>
