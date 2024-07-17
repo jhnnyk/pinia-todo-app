fafa4500dfba671b5020c3023ac834ebaf9f553a
John Knotts <jhnnyk@gmail.com>
2024-07-17 11:32:28 -0600

    styles

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -23,4 +23,35 @@ function addItemAndClear(item) {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+form {
+  margin-bottom: 15px;
+}
+
+input {
+  margin-top: 25px;
+  margin-bottom: 15px;
+  height: 20px;
+  width: 50%;
+}
+
+button {
+  margin-left: 30px;
+  background-color: #2f6089;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-weight: 800;
+  color: white;
+}
+
+.alert-div {
+  min-height: 25px;
+}
+
+.alert {
+  color: #d1495b;
+  font-size: 1em;
+  font-weight: 600;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -20,6 +20,24 @@ const { toggleCompleted, deleteTodo } = store
 </template>
 
 <style scoped>
+span {
+  margin: 0 10px;
+  cursor: pointer;
+}
+
+.item {
+  display: flex;
+  justify-content: center;
+}
+
+.content {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}
+
 .completed {
   text-decoration: line-through;
 }
