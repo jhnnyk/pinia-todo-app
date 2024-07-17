21bd49c9d654c5544286304b214d59dfc53f05f3
John Knotts <jhnnyk@gmail.com>
2024-07-17 10:43:29 -0600

    reactive properties in pinia

--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -2,6 +2,23 @@ import { defineStore } from 'pinia'
 
 export const useTodoListStore = defineStore('todoList', {
   state: () => ({
-    todoList: []
-  })
+    todoList: [],
+    id: 0
+  }),
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((todo) => {
+        return todo.id !== itemId
+      })
+    },
+    toggleCompleted(idToFind) {
+      const todo = this.todoList.find((obj) => obj.id === idToFind)
+      if (todo) {
+        todo.completed = !todo.completed
+      }
+    }
+  }
 })
